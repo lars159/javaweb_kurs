ef03be1cc7301858a275aa6b04f71586927a488c
Lars Carlsson <laca07@laca07-client.got.telia.se>
2015-10-28 13:52:11 +0100

    Added java

---
+++ part_2/src/main/resources/public/Ctrl.js
@@ -0,0 +1,13 @@
+var app = angular.module('app', []);
+
+app.controller('Ctrl', function($scope){
+<!-- REMOVE -->
+    $scope.isLoggedIn = false;
+   
+    $scope.login = function () {
+	$scope.isLoggedIn = true;
+    }
+
+
+
+});
---
+++ part_2/src/test/java/part_2/TestCtrl.java
@@ -0,0 +1,48 @@
+package part_2;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import se.cronit.Main;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Main.class)
+@WebAppConfiguration
+public class TestCtrl {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .build();
+    }
+
+    @Test
+    public void ok_signup_login() throws Exception {
+        mockMvc.perform(post("/intro/signup/lars?password=1234")).andExpect(status().isOk());
+        mockMvc.perform(get("/intro/login/lars?password=1234")).andExpect(status().isOk());
+    }
+    
+    @Test
+    public void error_login() throws Exception {
+        mockMvc.perform(get("/intro/login/error?password=1234")).andExpect(status().isForbidden());
+
+    }
+
+
+}
